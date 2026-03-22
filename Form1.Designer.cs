42240f850c47fb36e6adb026b9dce4ca85431fa4
Ahmed Sayed <88555626+FBI0NINJA@users.noreply.github.com>
2026-03-23 00:20:48 +0300

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,260 @@
+﻿namespace PasswordManagerPro
+{
+    partial class Form1
+    {
+        private System.ComponentModel.IContainer components = null;
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+                components.Dispose();
+            base.Dispose(disposing);
+        }
+
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.textBoxSite = new System.Windows.Forms.TextBox();
+            this.textBoxEmail = new System.Windows.Forms.TextBox();
+            this.textBoxPassword = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.buttonSave = new System.Windows.Forms.Button();
+            this.buttonDelete = new System.Windows.Forms.Button();
+            this.buttonCopy = new System.Windows.Forms.Button();
+            this.buttonShow = new System.Windows.Forms.Button();
+            this.buttonGithub = new System.Windows.Forms.Button();
+            this.buttonFacebook = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.buttonEdit = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // listBox1
+            // 
+            this.listBox1.BackColor = System.Drawing.Color.White;
+            this.listBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.listBox1.Font = new System.Drawing.Font("Microsoft YaHei", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.listBox1.ItemHeight = 19;
+            this.listBox1.Location = new System.Drawing.Point(12, 12);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(404, 363);
+            this.listBox1.TabIndex = 0;
+            this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+            // 
+            // textBoxSite
+            // 
+            this.textBoxSite.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.textBoxSite.Location = new System.Drawing.Point(519, 98);
+            this.textBoxSite.Name = "textBoxSite";
+            this.textBoxSite.Size = new System.Drawing.Size(250, 20);
+            this.textBoxSite.TabIndex = 1;
+            this.textBoxSite.TextChanged += new System.EventHandler(this.textBoxSite_TextChanged);
+            // 
+            // textBoxEmail
+            // 
+            this.textBoxEmail.Location = new System.Drawing.Point(519, 149);
+            this.textBoxEmail.Name = "textBoxEmail";
+            this.textBoxEmail.Size = new System.Drawing.Size(250, 20);
+            this.textBoxEmail.TabIndex = 2;
+            // 
+            // textBoxPassword
+            // 
+            this.textBoxPassword.Location = new System.Drawing.Point(519, 198);
+            this.textBoxPassword.Name = "textBoxPassword";
+            this.textBoxPassword.Size = new System.Drawing.Size(202, 20);
+            this.textBoxPassword.TabIndex = 3;
+            this.textBoxPassword.UseSystemPasswordChar = true;
+            // 
+            // label1
+            // 
+            this.label1.Font = new System.Drawing.Font("Simplified Arabic Fixed", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(447, 98);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(66, 23);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Site";
+            // 
+            // label2
+            // 
+            this.label2.Font = new System.Drawing.Font("Simplified Arabic Fixed", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(441, 149);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(76, 23);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Email";
+            // 
+            // label3
+            // 
+            this.label3.Font = new System.Drawing.Font("Simplified Arabic Fixed", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(422, 198);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(103, 20);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Password";
+            // 
+            // buttonSave
+            // 
+            this.buttonSave.BackColor = System.Drawing.Color.White;
+            this.buttonSave.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.buttonSave.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonSave.Location = new System.Drawing.Point(431, 259);
+            this.buttonSave.Name = "buttonSave";
+            this.buttonSave.Size = new System.Drawing.Size(86, 35);
+            this.buttonSave.TabIndex = 7;
+            this.buttonSave.Text = "Save";
+            this.buttonSave.UseVisualStyleBackColor = false;
+            this.buttonSave.Click += new System.EventHandler(this.buttonSave_Click);
+            // 
+            // buttonDelete
+            // 
+            this.buttonDelete.BackColor = System.Drawing.Color.White;
+            this.buttonDelete.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.buttonDelete.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonDelete.Location = new System.Drawing.Point(615, 259);
+            this.buttonDelete.Name = "buttonDelete";
+            this.buttonDelete.Size = new System.Drawing.Size(86, 35);
+            this.buttonDelete.TabIndex = 8;
+            this.buttonDelete.Text = "Delete";
+            this.buttonDelete.UseVisualStyleBackColor = false;
+            this.buttonDelete.Click += new System.EventHandler(this.buttonDelete_Click);
+            // 
+            // buttonCopy
+            // 
+            this.buttonCopy.BackColor = System.Drawing.Color.White;
+            this.buttonCopy.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.buttonCopy.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonCopy.Location = new System.Drawing.Point(523, 259);
+            this.buttonCopy.Name = "buttonCopy";
+            this.buttonCopy.Size = new System.Drawing.Size(86, 35);
+            this.buttonCopy.TabIndex = 9;
+            this.buttonCopy.Text = "Copy";
+            this.buttonCopy.UseVisualStyleBackColor = false;
+            this.buttonCopy.Click += new System.EventHandler(this.buttonCopy_Click);
+            // 
+            // buttonShow
+            // 
+            this.buttonShow.BackColor = System.Drawing.Color.White;
+            this.buttonShow.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.buttonShow.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.buttonShow.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonShow.Location = new System.Drawing.Point(727, 196);
+            this.buttonShow.Name = "buttonShow";
+            this.buttonShow.Size = new System.Drawing.Size(42, 28);
+            this.buttonShow.TabIndex = 10;
+            this.buttonShow.Text = "👁";
+            this.buttonShow.UseVisualStyleBackColor = false;
+            this.buttonShow.Click += new System.EventHandler(this.buttonShow_Click);
+            // 
+            // buttonGithub
+            // 
+            this.buttonGithub.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("buttonGithub.BackgroundImage")));
+            this.buttonGithub.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.buttonGithub.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.buttonGithub.FlatAppearance.BorderSize = 0;
+            this.buttonGithub.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.buttonGithub.Location = new System.Drawing.Point(648, 322);
+            this.buttonGithub.Name = "buttonGithub";
+            this.buttonGithub.Size = new System.Drawing.Size(121, 41);
+            this.buttonGithub.TabIndex = 12;
+            this.buttonGithub.UseVisualStyleBackColor = true;
+            this.buttonGithub.Click += new System.EventHandler(this.buttonGithub_Click);
+            // 
+            // buttonFacebook
+            // 
+            this.buttonFacebook.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("buttonFacebook.BackgroundImage")));
+            this.buttonFacebook.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.buttonFacebook.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.buttonFacebook.FlatAppearance.BorderSize = 0;
+            this.buttonFacebook.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.buttonFacebook.Location = new System.Drawing.Point(452, 322);
+            this.buttonFacebook.Name = "buttonFacebook";
+            this.buttonFacebook.Size = new System.Drawing.Size(121, 41);
+            this.buttonFacebook.TabIndex = 11;
+            this.buttonFacebook.UseVisualStyleBackColor = true;
+            this.buttonFacebook.Click += new System.EventHandler(this.buttonFacebook_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("MS UI Gothic", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.Color.Red;
+            this.label4.Location = new System.Drawing.Point(447, 20);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(328, 27);
+            this.label4.TabIndex = 13;
+            this.label4.Text = "Password Manager Pro 🔐";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Mongolian Baiti", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.ForeColor = System.Drawing.Color.Green;
+            this.label5.Location = new System.Drawing.Point(448, 47);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(143, 20);
+            this.label5.TabIndex = 14;
+            this.label5.Text = "by/Ahmed Sayed";
+            // 
+            // buttonEdit
+            // 
+            this.buttonEdit.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.buttonEdit.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.buttonEdit.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonEdit.Location = new System.Drawing.Point(707, 259);
+            this.buttonEdit.Name = "buttonEdit";
+            this.buttonEdit.Size = new System.Drawing.Size(75, 35);
+            this.buttonEdit.TabIndex = 15;
+            this.buttonEdit.Text = "Edit";
+            this.buttonEdit.UseVisualStyleBackColor = false;
+            this.buttonEdit.Click += new System.EventHandler(this.buttonEdit_Click);
+            // 
+            // Form1
+            // 
+            this.AutoValidate = System.Windows.Forms.AutoValidate.EnableAllowFocusChange;
+            this.ClientSize = new System.Drawing.Size(812, 385);
+            this.Controls.Add(this.buttonEdit);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.buttonGithub);
+            this.Controls.Add(this.buttonFacebook);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.textBoxSite);
+            this.Controls.Add(this.textBoxEmail);
+            this.Controls.Add(this.textBoxPassword);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.buttonSave);
+            this.Controls.Add(this.buttonDelete);
+            this.Controls.Add(this.buttonCopy);
+            this.Controls.Add(this.buttonShow);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Password Manager Pro 🔐";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.TextBox textBoxSite;
+        private System.Windows.Forms.TextBox textBoxEmail;
+        private System.Windows.Forms.TextBox textBoxPassword;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button buttonSave;
+        private System.Windows.Forms.Button buttonDelete;
+        private System.Windows.Forms.Button buttonCopy;
+        private System.Windows.Forms.Button buttonShow;
+        private System.Windows.Forms.Button buttonFacebook;
+        private System.Windows.Forms.Button buttonGithub;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button buttonEdit;
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,202 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
+using System.Windows.Forms;
+
+namespace PasswordManagerPro
+{
+    public partial class Form1 : Form
+    {
+        List<PasswordItem> items = new List<PasswordItem>();
+        string filePath = "data.json";
+        bool isVisible = false;
+
+        public Form1()
+        {
+            InitializeComponent();
+            this.FormClosing += Form1_FormClosing;
+        }
+
+        void LoadData()
+        {
+            if (File.Exists(filePath))
+            {
+                string json = File.ReadAllText(filePath);
+                items = JsonConvert.DeserializeObject<List<PasswordItem>>(json) ?? new List<PasswordItem>();
+
+                listBox1.Items.Clear();
+
+                foreach (var item in items)
+                    listBox1.Items.Add($"{item.Site} | {item.Email} | {item.Password}");
+            }
+        }
+
+
+        //json
+        void SaveData()
+        {
+            string json = JsonConvert.SerializeObject(items, Formatting.Indented);
+            File.WriteAllText(filePath, json);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            LoadData();
+        }
+
+
+        // save
+
+        private void buttonSave_Click(object sender, EventArgs e)
+        {
+            PasswordItem item = new PasswordItem()
+            {
+                Site = textBoxSite.Text,
+                Email = textBoxEmail.Text,
+                Password = textBoxPassword.Text
+            };
+
+            items.Add(item);
+            SaveData();
+            LoadData();
+
+            textBoxSite.Clear();
+            textBoxEmail.Clear();
+            textBoxPassword.Clear();
+        }
+
+
+        // list box
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            int index = listBox1.SelectedIndex;
+
+            if (index >= 0)
+            {
+                textBoxSite.Text = items[index].Site;
+                textBoxEmail.Text = items[index].Email;
+                textBoxPassword.Text = items[index].Password;
+            }
+        }
+
+
+        // delete
+        private void buttonDelete_Click(object sender, EventArgs e)
+        {
+            int index = listBox1.SelectedIndex;
+
+            if (index >= 0)
+            {
+                DialogResult result = MessageBox.Show(
+                    "هل انت متأكد من حذف هذا العنصر؟",
+                    "تأكيد الحذف",
+                    MessageBoxButtons.YesNo,
+                    MessageBoxIcon.Warning
+                );
+
+                if (result == DialogResult.Yes)
+                {
+                    items.RemoveAt(index);
+                    SaveData();
+                    LoadData();
+                }
+            }
+            else
+            {
+                MessageBox.Show("اختار عنصر الأول");
+            }
+        }
+
+
+        //copy
+        private void buttonCopy_Click(object sender, EventArgs e)
+        {
+            Clipboard.SetText(textBoxPassword.Text);
+            MessageBox.Show("Copied!");
+        }
+
+        //اظهار-اخفاء كلمة السر
+        private void buttonShow_Click(object sender, EventArgs e)
+        {
+            isVisible = !isVisible;
+            textBoxPassword.UseSystemPasswordChar = !isVisible;
+        }
+
+
+        //زرار فتح الفيس بوك
+        private void buttonFacebook_Click(object sender, EventArgs e)
+        {
+            System.Diagnostics.Process.Start("https://www.facebook.com/ninjafbi1");
+        }
+
+
+        //زرار فتح جيتهاب
+        private void buttonGithub_Click(object sender, EventArgs e)
+        {
+            System.Diagnostics.Process.Start("https://github.com/FBI0NINJA");
+
+        }
+
+        private void buttonFacebook_MouseEnter(object sender, EventArgs e)
+        {
+            buttonFacebook.BackColor = Color.LightGray;
+        }
+
+        private void buttonFacebook_MouseLeave(object sender, EventArgs e)
+        {
+            buttonFacebook.BackColor = Color.Transparent;
+        }
+
+        //فتح موقع عند الخروج
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            System.Diagnostics.Process.Start(new System.Diagnostics.ProcessStartInfo
+            {
+                FileName = "https://www.facebook.com/ninjafbi1",
+                UseShellExecute = true
+            });
+        }
+
+        private void label5_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label6_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBoxSite_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+
+        // edit
+        private void buttonEdit_Click(object sender, EventArgs e)
+        {
+            int index = listBox1.SelectedIndex;
+
+            if (index >= 0)
+            {
+               
+                items[index].Site = textBoxSite.Text;
+                items[index].Email = textBoxEmail.Text;
+                items[index].Password = textBoxPassword.Text;
+
+                SaveData();
+                LoadData();
+
+                MessageBox.Show("تم التعديل بنجاح ✅");
+            }
+            else
+            {
+                MessageBox.Show("اختار عنصر الأول");
+            }
+        }
+    }
+}
